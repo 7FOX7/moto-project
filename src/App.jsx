5c982e9a1aae1b6247cbc535a58b920bc6d9111b
7FOX7 <kheeugene@gmail.com>
2024-09-14 12:01:05 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Hello from App.jsx</p>
+    </>
+  )
+}
+
+export default App
+
+/*  
+    the goal: 
+  1. Create the loading bar which will just be loading every second. 
+  2. Once you have this, try to load it customizeably, like: 10%, then 43%, then 70%, then 100%; 
+*/
