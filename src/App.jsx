8cdd04fd9e6db705030c7a2b4a34be00f2baa4d1
7FOX7 <kheeugene@gmail.com>
2024-09-15 18:30:53 -0400

    Added lazy loading of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import './App.css'
+import { lazy } from 'react'
+import { Suspense } from 'react'
 import { Routes } from 'react-router-dom'
-import { Route } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { ScreenSizeProvider } from './contexts/ScreenSizeContext'
-import Onboarding from './pages/Onboarding'
-import Home from './pages/Home'
+import artificialDelay from './utils/functions/artificialDelay'
+import CustomLoadingBar from './customs/components/CustomLoadingBar'
+
+const Onboarding = lazy(() => artificialDelay(5000, import('./pages/Onboarding')))
+const Home = lazy(() => import('./pages/Home'))
+const Layout = lazy(() => import('./components/Layout/Layout'))
 
 function App() {
   return (
     <ScreenSizeProvider>
-      <Routes>
-        <Route path="/" element={<Navigate to="/onboarding" />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <Suspense fallback={<CustomLoadingBar />}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/onboarding" />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="onboarding" element={<Onboarding />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </ScreenSizeProvider>
   )
 }
