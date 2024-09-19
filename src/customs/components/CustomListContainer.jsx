0ecd5c027acb61a635982aaae38d814cee3a865c
7FOX7 <kheeugene@gmail.com>
2024-09-19 14:59:44 -0400

    Completed working on Footer

--- src/customs/components/CustomListContainer.jsx
+++ src/customs/components/CustomListContainer.jsx
@@ -1,10 +1,14 @@
 import { useScreenSize } from "../../contexts/ScreenSizeContext"
+import CustomNavLink from "./CustomNavLink"
+import CustomLinkIcon from "./CustomLinkIcon"
+import CustomText from "./CustomText"
 import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
+import Grid from '@mui/material/Grid2'
 import navLinks from "../../data/navLinks"
-import Typography from "@mui/material/Typography"
-import CustomNavLink from "./CustomNavLink"
+import Divider from "@mui/material/Divider"
+import Box from "@mui/material/Box"
+import networkLinks from "../../data/networkLinks"
 
 const CustomListContainer = {
    header: () => {
@@ -16,10 +20,11 @@ const CustomListContainer = {
             {navLinks.map((navLink) => {
                return (
                   <ListItem key={navLink.id}>
-                     {/* <ListItemButton>
-                        <Typography sx={{textWrap: "nowrap"}}>{navLink.title}</Typography>
-                     </ListItemButton> */}
-                     <CustomNavLink path={navLink.path} title={navLink.title} bold={false} />
+                     <CustomNavLink 
+                        path={navLink.path} 
+                        title={navLink.title}
+                        bold={true}    
+                     />
                   </ListItem>
                )
             })}
@@ -27,150 +32,107 @@ const CustomListContainer = {
       )
    }, 
    footer: () => {
-      const isSmallScreen = useScreenSize()
       return (
-         <List>
-            <img
-               src="/images/fox_logo.png" 
-               style={{
-                  width: "200px", 
-                  height: "200px", 
-               }} 
-            />
-            {navLinks.map((navLink) => {
-               return (
-                  <ListItem key={navLink.id}>
-                     {/* <CustomNavLink path={navLink.path} text={navLink.title} bold={true} />
-                     {navLink.subLinks && navLink.subLinks.length > 0 && (
-                        <List>
-                           {navLink.subLinks.map((subLink) => {
+         <Box sx={{
+            padding: "20px 25px"
+         }}>
+            <Grid
+            container
+            columns={12}
+            size={12}
+         >
+            <Grid 
+               size={{xs: 12, sm: 12, md: 4, lg: 4, xl: 4}}
+            >
+               <img
+                  src="/images/fox_logo.png" 
+                  draggable="false"
+                  style={{
+                     width: "100px", 
+                     height: "100px", 
+                  }} 
+               />
+            </Grid>
+            <Grid 
+               container 
+               columns={12}
+               columnSpacing={12}
+               rowSpacing={3}
+               size={{xs: 12, sm: 12, md: 8, lg: 8, xl: 8}}
+            >
+               {navLinks.map((navLink) => {
+                  return (
+                        <Grid 
+                        key={navLink.id} 
+                        size={{xs: 12, sm: 6, md: 2, lg: 2, xl: 2}}
+                     >
+                        <CustomNavLink 
+                           path={navLink.path} 
+                           title={navLink.title}
+                           bold={true} 
+                        />
+                        {navLink.subLinks && navLink.subLinks.length > 0 && (
+                           navLink.subLinks.map((subLink) => {
                               return (
-                                 <ListItem key={subLink.id}>
-                                    <CustomNavLink path={subLink.path} text={subLink.title} bold={false} />
-                                 </ListItem>
+                                 <Grid 
+                                    key={subLink.id}
+                                 >
+                                    <CustomNavLink 
+                                       path={subLink.path} 
+                                       title={subLink.title} 
+                                       secondary={true} 
+                                    />
+                                 </Grid>
                               )
-                           })}
-                        </List>
-                     )} */}
-                     <ListItemButton>
-                        {navLink.title}
-                     </ListItemButton>
-                  </ListItem>
-               )
-            })}
-         </List>
+                           })
+                        )}
+                     </Grid>
+                  )
+               })}
+            </Grid>
+         </Grid>
+         <br />
+         <Divider orientation="horizontal" sx={{background: "white"}} />
+         <br />
+         {/* TODO: Create a custom component that will contain the text */}
+         <Grid
+            container
+            columns={12}
+            size={12}
+            alignItems="center"
+         >
+            <Grid 
+               size={{xs: 12, sm: 12, md: 8, lg: 8, xl: 8}}
+            >
+               <CustomText 
+                  text="Copyright &copy; 2024 7FOX7. All rights reserved." 
+                  secondary={true}
+                  variant="body2"
+               />
+            </Grid>
+            <Grid
+               container
+               columnSpacing={{xs: 6, sm: 6, md: 6, lg: 6, xl: 2}}
+               size={{xs: 12, sm: 8, md: 4, lg: 4, xl: 4}}
+            >
+               {networkLinks.map((networkLink) => {
+                  return (
+                     <Grid 
+                        key={networkLink.id} 
+                        size={1}
+                     >
+                        <CustomLinkIcon 
+                           url={networkLink.url} 
+                           href={networkLink.href} 
+                        />
+                     </Grid>
+                  )
+               })}
+            </Grid>
+         </Grid>
+         </Box>
       )
    }
 }
 
 export default CustomListContainer
-
-/*
-   assume I have an array which contains the nav links: 
-
-   const navLinks = [
-   {
-      id: "workoutLink", 
-      title: "Workout", 
-      path: "/workout", 
-      subLinks: [
-         {
-            id: "workoutVideos", 
-            title: "Videos", 
-            path: "/videos"
-         }, 
-         {
-            id: "workoutPrograms", 
-            title: "Programs", 
-            path: "/programs"
-         }, 
-      ] 
-   },
-   {
-      ... other links
-   } 
-
-   and, I have a Component which displays the links in the List. The code looks like this: 
-
-   const CustomListContainer = () => {
-      return (
-         <List>
-            {navLinks.map((navLink) => {
-               return (
-                  <ListItem key={navLink.id}>
-                     <ListItemButton>
-                        <Typography sx={{textWrap: "nowrap"}}>{navLink.title}</Typography>
-                     </ListItemButton>
-                  </ListItem>
-               )
-            })}
-         </List>
-      )   
-   }
-
-   but, what if I want to also display the sublinks? The code could look like this: 
-
-   const CustomListContainer = () => {
-      return (
-         <List>
-            {navLinks.map((navLink) => {
-               return (
-                  <ListItem key={navLink.id}>
-                     <ListItemButton>
-                        <Typography sx={{textWrap: "nowrap"}}>{navLink.title}</Typography>
-                     </ListItemButton>
-                     {navLink.map((subLink) => {
-                        <ListItemButton>
-                           <Typography sx={{textWrap: "nowrap"}}>{subLink.title}</Typography>
-                        </ListItemButton>
-                     })}
-                  </ListItem>
-               )
-            })}
-         </List>
-      )   
-   }
-
-   the question is: should I create a different ListItem for the subLink, or I should fit both 
-   mainLink and subLink inside a single ListItem? The code with the separate ListItem can look like: 
-
-   const CustomListContainer = () => {
-      return (
-         <List>
-            {navLinks.map((navLink) => {
-               return (
-                  <ListItem key={navLink.id}>
-                     <ListItemButton>
-                        <Typography sx={{textWrap: "nowrap"}}>{navLink.title}</Typography>
-                     </ListItemButton>
-                  </ListItem>
-                  {navLink.map((subLink) => {
-                     <ListItem key={subLink.id}>
-                        <ListItemButton>
-                           <Typography sx={{textWrap: "nowrap"}}>{subLink.title}</Typography>
-                        </ListItemButton>
-                     </ListItem>
-                  })}
-               )
-            })}
-         </List>
-      )   
-   }
-
-   <ListItem key={navLink.id}>
-      <ListItemButton>
-         <Typography sx={{ textWrap: 'nowrap' }}>{navLink.title}</Typography>
-      </ListItemButton>
-      {navLink.subLinks && navLink.subLinks.length > 0 && (
-         <List component="div" disablePadding>
-            {navLink.subLinks.map((subLink) => (
-               <ListItem key={subLink.id}>
-                  <ListItemButton>
-                     <Typography sx={{ textWrap: 'nowrap' }}>{subLink.title}</Typography>
-                  </ListItemButton>
-               </ListItem>
-            ))}
-         </List>
-      )}
-   </ListItem>
-*/
