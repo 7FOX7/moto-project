a439b7bd9796548a2fa31ba58f73020184e35d07
7FOX7 <kheeugene@gmail.com>
2024-09-19 14:58:43 -0400

    Added 'CustomText' component and modified theme accordingly

---
+++ src/customs/components/CustomText.jsx
@@ -0,0 +1,19 @@
+import { memo } from "react"
+import Typography from "@mui/material/Typography"
+
+const CustomText = memo(function CustomText(props) {
+   const {text, secondary=false, bold=false, variant="body1"} = props
+   return (
+      <>
+         <Typography 
+            color={secondary ? "global.secondaryTextContent.main" : "primary"}
+            variant={variant}
+            typography={bold && "global.boldText"}
+         >
+            {text}
+         </Typography>
+      </>
+   )
+})
+
+export default CustomText
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,6 +1,15 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
+   components: {
+      MuiDrawer: {
+         styleOverrides: {
+            paper: {
+               background: "linear-gradient(90deg, rgba(33,33,33,1) 0%, rgba(47,47,47,1) 89%)"
+            }
+         }
+      }
+   }, 
    palette: {
       primary: {
          main: "rgb(255, 255, 255)",  
@@ -8,13 +17,11 @@ const theme = createTheme({
       global: {
          borderLinearProgress: {
             main: "linear-gradient(90deg, rgba(0,0,73,1) 0%, rgba(0,137,255,1) 100%)"
-         }
-      }, 
-      footer: {
-         subLink: {
+         }, 
+         secondaryTextContent: {
             main: "rgba(200, 200, 200, 0.5)"
          }
-      }
+      }, 
    }, 
    shape: {
       borderLinearProgress: {
@@ -22,11 +29,11 @@ const theme = createTheme({
       }
    }, 
    typography: {
-      footer: {
-         mainLink: {
+      global: {
+         boldText: {
             fontWeight: "700"
          }
-      }, 
+      }
    }
 })
 
