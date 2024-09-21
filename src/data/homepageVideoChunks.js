23486532d8505edefdb2f31310cef0933b2a5a68
7FOX7 <kheeugene@gmail.com>
2024-09-21 19:30:49 -0400

    Added video chunks (which will be used in Home page) to the project

---
+++ src/data/homepageVideoChunks.js
@@ -0,0 +1,48 @@
+const homepageVideoChunks = [
+   {
+      id: "motorcycleClip0", 
+      src: "/videos/motorcycleClip1.mp4"
+   }, 
+   {
+      id: "motorcycleClip1", 
+      src: "/videos/motorcycleClip2.mp4"
+   }, 
+   {
+      id: "motorcycleClip2", 
+      src: "/videos/motorcycleClip3.mp4"
+   }, 
+   {
+      id: "motorcycleClip3", 
+      src: "/videos/motorcycleClip4.mp4"
+   }, 
+   {
+      id: "motorcycleClip4", 
+      src: "/videos/motorcycleClip5.mp4"
+   }, 
+   {
+      id: "motorcycleClip5", 
+      src: "/videos/motorcycleClip6.mp4"
+   }, 
+   {
+      id: "motorcycleClip6", 
+      src: "/videos/motorcycleClip7.mp4"
+   }, 
+   {
+      id: "motorcycleClip7", 
+      src: "/videos/motorcycleClip8.mp4"
+   }, 
+   {
+      id: "motorcycleClip8", 
+      src: "/videos/motorcycleClip9.mp4"
+   }, 
+   {
+      id: "motorcycleClip9", 
+      src: "/videos/motorcycleClip10.mp4"
+   }, 
+   {
+      id: "motorcycleClip10", 
+      src: "/videos/motorcycleClip11.mp4"
+   }, 
+]
+
+export default homepageVideoChunks
