dc6e15c3847c0a3cc63a56847d28902fe9263662
7FOX7 <kheeugene@gmail.com>
2024-09-22 00:46:50 -0400

    Added an additional text to the video chunks

--- src/data/homepageVideoChunks.js
+++ src/data/homepageVideoChunks.js
@@ -1,47 +1,58 @@
 const homepageVideoChunks = [
    {
       id: "motorcycleClip0", 
-      src: "/videos/motorcycleClip1.mp4"
+      src: "/videos/motorcycleClip1.mp4", 
+      addText: "Still something to prove"
    }, 
    {
       id: "motorcycleClip1", 
-      src: "/videos/motorcycleClip2.mp4"
+      src: "/videos/motorcycleClip2.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
    {
       id: "motorcycleClip2", 
-      src: "/videos/motorcycleClip3.mp4"
+      src: "/videos/motorcycleClip3.mp4", 
+      addText: "Yes, there was Still something to prove, or not?"
    }, 
    {
       id: "motorcycleClip3", 
-      src: "/videos/motorcycleClip4.mp4"
+      src: "/videos/motorcycleClip4.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
    {
       id: "motorcycleClip4", 
-      src: "/videos/motorcycleClip5.mp4"
+      src: "/videos/motorcycleClip5.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
    {
       id: "motorcycleClip5", 
-      src: "/videos/motorcycleClip6.mp4"
+      src: "/videos/motorcycleClip6.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
    {
       id: "motorcycleClip6", 
-      src: "/videos/motorcycleClip7.mp4"
+      src: "/videos/motorcycleClip7.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
    {
       id: "motorcycleClip7", 
-      src: "/videos/motorcycleClip8.mp4"
+      src: "/videos/motorcycleClip8.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
    {
       id: "motorcycleClip8", 
-      src: "/videos/motorcycleClip9.mp4"
+      src: "/videos/motorcycleClip9.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
    {
       id: "motorcycleClip9", 
-      src: "/videos/motorcycleClip10.mp4"
+      src: "/videos/motorcycleClip10.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
    {
       id: "motorcycleClip10", 
-      src: "/videos/motorcycleClip11.mp4"
+      src: "/videos/motorcycleClip11.mp4", 
+      addText: "Yes, there was Still something to prove"
    }, 
 ]
 
