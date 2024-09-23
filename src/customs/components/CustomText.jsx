2c24a28cb8cf88abdad7e4253904083d0b89b303
7FOX7 <kheeugene@gmail.com>
2024-09-22 22:19:43 -0400

    Added an additional prop to the 'CustomText' component

--- src/customs/components/CustomText.jsx
+++ src/customs/components/CustomText.jsx
@@ -2,13 +2,20 @@ import { memo } from "react"
 import Typography from "@mui/material/Typography"
 
 const CustomText = memo(function CustomText(props) {
-   const {text, secondary=false, bold=false, variant="body1"} = props
+   const {
+      text, 
+      secondary=false, 
+      bold=false, 
+      variant="body1", 
+      fontSize
+   } = props
    return (
       <>
          <Typography 
             color={secondary ? "global.secondaryTextContent.main" : "primary"}
             variant={variant}
             typography={bold && "global.boldText"}
+            fontSize={fontSize}
             sx={{
                textAlign: "center"
             }}
