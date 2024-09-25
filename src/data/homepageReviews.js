e781101a3c56910354987f495e59a197e968a3a9
7FOX7 <kheeugene@gmail.com>
2024-09-25 02:11:15 -0400

    Added an array which will store homepage reviews data

---
+++ src/data/homepageReviews.js
@@ -0,0 +1,60 @@
+const homepageReviews = [
+   {
+      id: "review1", 
+      publisher: "Tim L", 
+      value: 4, 
+      publishDate: "2024-09-24", 
+      content: "They are good professionals. No Regrets!"
+   }, 
+   {
+      id: "review2", 
+      publisher: "Simona A",
+      value: 5,  
+      publishDate: "2024-11-07", 
+      content: "Best!"
+   }, 
+   {
+      id: "review3", 
+      publisher: "Paul K",
+      value: 4, 
+      publishDate: "2024-04-19", 
+      content: "If you need help, they will help you!"
+   }, 
+   {
+      id: "review4", 
+      publisher: "Umar O",
+      value: 4, 
+      publishDate: "2024-05-23", 
+      content: "Good service."
+   }, 
+   {
+      id: "review5", 
+      publisher: "Ben F",
+      value: 5, 
+      publishDate: "2024-03-25", 
+      content: "I used their service once. And Im completely satisfied!"
+   }, 
+   {
+      id: "review6", 
+      publisher: "Mark B",
+      value: 5, 
+      publishDate: "2024-01-10", 
+      content: "Awesome!"
+   }, 
+   {
+      id: "review7", 
+      publisher: "John D",
+      value: 5, 
+      publishDate: "2024-03-23", 
+      content: "Highly recommend them if you need help with something"
+   }, 
+   {
+      id: "review8", 
+      publisher: "Ricky G",
+      value: 5, 
+      publishDate: "2024-04-17", 
+      content: "These guys know what they're doing!"
+   }, 
+]
+
+export default homepageReviews
