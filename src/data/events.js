27a1d92deed60382657fcee91f2d57e3c4de516c
Evgenii Khe <ekhe1@ncstudents.niagaracollege.ca>
2024-09-27 22:08:51 -0400

    Added source images which will be used as a video thumbnail/event background image in the 'Rides' route

---
+++ src/data/events.js
@@ -0,0 +1,94 @@
+const events = [
+   {
+      id: "event1", 
+      title: "Default Event title 1", 
+      date: "2024-01-10", 
+      place: "New-York City", 
+      description: "There was still something to prove", 
+      imgSrc: "/images/event-social-ride.webp",
+      imgAlt: "Event Background Image",  
+      type: "Social"
+   }, 
+   {
+      id: "event2", 
+      title: "Default Event title 2",
+      date: "2024-01-10", 
+      place: "New-York City",  
+      description: "There was still something to prove",
+      imgSrc: "/images/event-workshop-ride.jpg",
+      imgAlt: "Event Background Image",   
+      type: "Workshop"
+   }, 
+   {
+      id: "event3", 
+      title: "Default Event title 1",
+      date: "2024-01-10", 
+      place: "New-York City",  
+      description: "There was still something to prove",
+      imgSrc: "/images/event-social-ride.webp",
+      imgAlt: "Event Background Image", 
+      type: "Social" 
+   }, 
+   {
+      id: "event4", 
+      title: "Default Event title 2",
+      date: "2024-01-10", 
+      place: "New-York City",  
+      description: "There was still something to prove",
+      imgSrc: "/images/event-social-ride.webp",
+      imgAlt: "Event Background Image",  
+      type: "Social"
+   }, 
+   {
+      id: "event5", 
+      title: "Default Event title 1",
+      date: "2024-01-10", 
+      place: "New-York City",  
+      description: "There was still something to prove",
+      imgSrc: "/images/event-charity-ride.jpg",
+      imgAlt: "Event Background Image",  
+      type: "Charity"
+   }, 
+   {
+      id: "event6", 
+      title: "Default Event title 2",
+      date: "2024-01-10", 
+      place: "New-York City",  
+      description: "There was still something to prove",
+      imgSrc: "/images/event-social-ride.webp",
+      imgAlt: "Event Background Image",  
+      type: "Social"
+   }, 
+   {
+      id: "event7", 
+      title: "Default Event title 2",
+      date: "2024-01-10", 
+      place: "New-York City",  
+      description: "There was still something to prove", 
+      imgSrc: "/images/event-camping-ride.jpg",
+      imgAlt: "Event Background Image", 
+      type: "Camping"
+   },
+   {
+      id: "event8", 
+      title: "Default Event title 2",
+      date: "2024-01-10", 
+      place: "New-York City",  
+      description: "There was still something to prove",
+      imgSrc: "/images/event-racing-ride.webp",
+      imgAlt: "Event Background Image",  
+      type: "Racing"
+   },
+   {
+      id: "event9", 
+      title: "Default Event title 2",
+      date: "2024-01-10", 
+      place: "New-York City",  
+      description: "There was still something to prove",
+      imgSrc: "/images/event-racing-ride.webp",
+      imgAlt: "Event Background Image",  
+      type: "Racing"
+   },
+]
+
+export default events
---
+++ src/data/videoThumbnails.js
@@ -0,0 +1,32 @@
+const videoThumbnails = [
+   {
+      id: "videoThumbnail1",
+      src: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
+   }, 
+   {
+      id: "videoThumbnail3",
+      src: "/images/thumbnail-common-mistakes.jpg", 
+   }, 
+   {
+      id: "videoThumbnail4",
+      src: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
+   }, 
+   {
+      id: "videoThumbnail5",
+      src: "/images/thumbnail-common-mistakes.jpg", 
+   }, 
+   {
+      id: "videoThumbnail6",
+      src: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
+   }, 
+   {
+      id: "videoThumbnail7",
+      src: "/images/thumbnail-common-mistakes.jpg", 
+   }, 
+   {
+      id: "videoThumbnail8",
+      src: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
+   }, 
+]
+
+export default videoThumbnails
