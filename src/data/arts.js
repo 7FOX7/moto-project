a4816b35f1d47bf9174178b8a3a5feb5bca3c783
7FOX7 <kheeugene@gmail.com>
2024-09-29 12:11:28 -0400

    Added an array for containing art data

---
+++ src/data/arts.js
@@ -0,0 +1,49 @@
+const arts = [
+   {
+      id: "art1", 
+      imgSrc: "/images/art-biker-showing-hand.jpeg", 
+      imgAlt: "Biker showing hand"
+   }, 
+   {
+      id: "art2", 
+      imgSrc: "/images/art-biker-riding-touring-motorcycle-jumps.webp", 
+      imgAlt: "Biker riding touring motorcycle jumps"
+   },
+   {
+      id: "art3", 
+      imgSrc: "/images/art-dirt-bike.jpg", 
+      imgAlt: "Dirt bike"
+   }, 
+   {
+      id: "art4", 
+      imgSrc: "/images/art-empty-motorcycle.jpg", 
+      imgAlt: "Empty motorcycle"
+   }, 
+   {
+      id: "art5", 
+      imgSrc: "/images/art-girl-on-motorcycle.webp", 
+      imgAlt: "Girl on motorcycle"
+   }, 
+   {
+      id: "art6", 
+      imgSrc: "/images/art-idle-biker.jpg", 
+      imgAlt: "Idle biker"
+   }, 
+   {
+      id: "art7", 
+      imgSrc: "/images/art-man-motorcycle.jpg", 
+      imgAlt: "Man riding motorcycle"
+   }, 
+   {
+      id: "art8", 
+      imgSrc: "/images/art-standing-girl.jpg", 
+      imgAlt: "Standing girl biker"
+   }, 
+   {
+      id: "art9", 
+      imgSrc: "/images/art-empty-motorcycle.jpg", 
+      imgAlt: "Empty motorcycle"
+   }, 
+]
+
+export default arts
