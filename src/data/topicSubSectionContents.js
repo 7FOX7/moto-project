c863680013182cdeaefa0ff3656eac27809aef6f
7FOX7 <kheeugene@gmail.com>
2024-10-01 15:30:55 -0400

    Added a temporary array of contents for the sub sections in the topic section (Community route)

---
+++ src/data/topicSubSectionContents.js
@@ -0,0 +1,97 @@
+const topicSubSectionContents = [
+   {
+      id: "topicSubSectionContent1", 
+      title: "What's Hot", 
+      content: [
+         {
+            id: "topicHot1", 
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }, 
+         {
+            id: "topicHot2", 
+            imgSrc: "/images/art-dirt-bike.jpg", 
+            imgAlt: "Dirt bike"
+         }, 
+         {
+            id: "topicHot3", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg", 
+            imgAlt: "Biker showing hand"
+         }
+      ]
+   }, 
+   {
+      id: "topicSubSectionContent2", 
+      title: "What's New", 
+      content: [
+         {
+            id: "topicNew1", 
+            publishDate: "2024-09-24", 
+            publisher: "Mark Siva", 
+            title: "I wish I know about this game earlier!", 
+            category: "Books"
+         }, 
+         {
+            id: "topicNew2", 
+            publishDate: "2024-09-24", 
+            publisher: "John Doe", 
+            title: "I wish I know about this game earlier!", 
+            category: "Books"
+         }, 
+         {
+            id: "topicNew3", 
+            publishDate: "2024-09-24", 
+            publisher: "Luke Simba", 
+            title: "I wish I know about this game earlier!", 
+            category: "Books"
+         }, 
+         {
+            id: "topicNew4", 
+            publishDate: "2024-09-24", 
+            publisher: "Peter Parker", 
+            title: "I wish I know about this game earlier!", 
+            category: "Books"
+         }, 
+      ]
+   }, 
+   {
+      id: "topicSubSectionContent3", 
+      title: "Best of", 
+      content: [
+         {
+            id: "topicNew1", 
+            publishDate: "2024-09-24", 
+            publisher: "Thomas Yillah", 
+            title: "I wish I know about this game earlier!", 
+            category: "Books"
+         }, 
+         {
+            id: "topicNew2", 
+            publishDate: "2024-09-24", 
+            publisher: "Thomas Yillah", 
+            title: "I wish I know about this game earlier!", 
+            category: "Books"
+         }, 
+         {
+            id: "topicNew3", 
+            publishDate: "2024-09-24", 
+            publisher: "Thomas Yillah", 
+            title: "I wish I know about this game earlier!", 
+            category: "Books"
+         }, 
+         {
+            id: "topicNew4", 
+            publishDate: "2024-09-24", 
+            publisher: "Thomas Yillah", 
+            title: "I wish I know about this game earlier!", 
+            category: "Books"
+         }, 
+      ]
+   }
+]
+
+export default topicSubSectionContents
+
+/*
+   
+*/
