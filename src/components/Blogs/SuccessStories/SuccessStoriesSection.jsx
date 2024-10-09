3522c3d6f338a46872334a0c58c2b727994bb655
7FOX7 <kheeugene@gmail.com>
2024-10-09 02:19:06 -0400

    Adjusted the text

--- src/components/Blogs/SuccessStories/SuccessStoriesSection.jsx
+++ src/components/Blogs/SuccessStories/SuccessStoriesSection.jsx
@@ -5,9 +5,9 @@ const SuccessStoriesSection = () => {
    return (
       <CustomSection
          headingText="Success Stories"
-         subHeadingText="Our community is growing fast and. See what others have to share"
+         subHeadingText="Our community is growing fast. See what others have to share"
          content={<SuccessStoriesContent />}
-   />
+      />
    )
 }
 
