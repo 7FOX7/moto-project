6982f3e40c6b6e8bba3559e029c3980a777ad6ad
7FOX7 <kheeugene@gmail.com>
2024-10-12 22:58:48 -0400

    Added new properties

--- src/data/users.js
+++ src/data/users.js
@@ -2,89 +2,273 @@ const users = [
    {
       id: "soomeId1", 
       username: "7FOX7", 
+      email: "kheeugene@gmail.com", 
+      registrationDate: "10/21/2024", 
       likes: 123, 
-      description: "I'm passionate about frontend development. Feel free to connect!", 
+      bio: "I'm passionate about frontend development. Feel free to connect!", 
       interests: [
-         "AI", 
-         "Software", 
-         "Science", 
-         "Music"
+         {
+            id: "interest1", 
+            name: "AI",
+         },
+         {
+            id: "interest2", 
+            name: "Software",
+         },
+         {
+            id: "interest3", 
+            name: "Science",
+         },
+         {
+            id: "interest4", 
+            name: "Music",
+         },
+      ], 
+      arts: [
+         {
+            id: "userArt1", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg",
+            imgAlt: "Biker showing hand" 
+         }, 
+         {
+            id: "userArt2",
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }
       ]
    }, 
    {
       id: "soomeId3", 
       username: "Johnyy", 
+      email: "kheeugene@gmail.com", 
+      registrationDate: "10/21/2024", 
       likes: 468, 
-      description: "I'm passionate about backend development. Feel free to connect!", 
+      bio: "I'm passionate about backend development. Feel free to connect!", 
       interests: [
-         "Software", 
-         "Food"
+         {
+            id: "interest1", 
+            name: "Music",
+         },
+         {
+            id: "interest2", 
+            name: "Books",
+         },
+      ], 
+      arts: [
+         {
+            id: "userArt1", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg",
+            imgAlt: "Biker showing hand" 
+         }, 
+         {
+            id: "userArt2",
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }
       ]
    }, 
    {
       id: "soomeId4", 
       username: "TimTim", 
+      email: "kheeugene@gmail.com", 
+      registrationDate: "10/21/2024", 
       likes: 123, 
-      description: "I'm passionate about frontend development. Feel free to connect!", 
+      bio: "I'm passionate about frontend development. Feel free to connect!", 
       interests: [
-         "AI", 
-         "Software", 
-         "Science", 
-         "Music"
+         {
+            id: "interest1", 
+            name: "AI",
+         },
+         {
+            id: "interest2", 
+            name: "Software",
+         },
+         {
+            id: "interest3", 
+            name: "Science",
+         },
+         {
+            id: "interest4", 
+            name: "Music",
+         },
+      ], 
+      arts: [
+         {
+            id: "userArt1", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg",
+            imgAlt: "Biker showing hand" 
+         }, 
+         {
+            id: "userArt2",
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }
       ]
    }, 
    {
       id: "soomeId5", 
       username: "Rookie", 
+      email: "kheeugene@gmail.com", 
+      registrationDate: "10/21/2024", 
       likes: 468, 
-      description: "I'm passionate about backend development. Feel free to connect!", 
+      bio: "I'm passionate about backend development. Feel free to connect!", 
       interests: [
-         "Software", 
-         "Food"
+         {
+            id: "interest1", 
+            name: "Music",
+         },
+         {
+            id: "interest2", 
+            name: "Books",
+         },
+      ], 
+      arts: [
+         {
+            id: "userArt1", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg",
+            imgAlt: "Biker showing hand" 
+         }, 
+         {
+            id: "userArt2",
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }
       ]
    }, 
    {
       id: "soomeId6", 
       username: "Margo6", 
+      email: "kheeugene@gmail.com", 
+      registrationDate: "10/21/2024", 
       likes: 123, 
-      description: "I'm passionate about frontend development. Feel free to connect!", 
+      bio: "I'm passionate about frontend development. Feel free to connect!", 
       interests: [
-         "AI", 
-         "Software", 
-         "Science", 
-         "Music"
+         {
+            id: "interest1", 
+            name: "AI",
+         },
+         {
+            id: "interest2", 
+            name: "Software",
+         },
+         {
+            id: "interest3", 
+            name: "Science",
+         },
+         {
+            id: "interest4", 
+            name: "Music",
+         },
+      ], 
+      arts: [
+         {
+            id: "userArt1", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg",
+            imgAlt: "Biker showing hand" 
+         }, 
+         {
+            id: "userArt2",
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }
       ]
    }, 
    {
       id: "soomeId7", 
       username: "Angella", 
+      email: "kheeugene@gmail.com", 
+      registrationDate: "10/21/2024", 
       likes: 468, 
-      description: "I'm passionate about backend development. Feel free to connect!", 
+      bio: "I'm passionate about backend development. Feel free to connect!", 
       interests: [
-         "Software", 
-         "Food"
+         {
+            id: "interest1", 
+            name: "Music",
+         },
+         {
+            id: "interest2", 
+            name: "Books",
+         },
+      ], 
+      arts: [
+         {
+            id: "userArt1", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg",
+            imgAlt: "Biker showing hand" 
+         }, 
+         {
+            id: "userArt2",
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }
       ]
    }, 
    {
       id: "soomeId8", 
       username: "Simona", 
+      email: "kheeugene@gmail.com", 
+      registrationDate: "10/21/2024", 
       likes: 123, 
-      description: "I'm passionate about frontend development. Feel free to connect!", 
+      bio: "I'm passionate about frontend development. Feel free to connect!", 
       interests: [
-         "AI", 
-         "Software", 
-         "Science", 
-         "Music"
+         {
+            id: "interest1", 
+            name: "AI",
+         },
+         {
+            id: "interest2", 
+            name: "Software",
+         },
+         {
+            id: "interest3", 
+            name: "Science",
+         },
+         {
+            id: "interest4", 
+            name: "Music",
+         },
+      ], 
+      arts: [
+         {
+            id: "userArt1", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg",
+            imgAlt: "Biker showing hand" 
+         }, 
+         {
+            id: "userArt2",
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }
       ]
    }, 
    {
       id: "soomeId9", 
       username: "Kirska", 
+      email: "kheeugene@gmail.com", 
+      registrationDate: "10/21/2024", 
       likes: 468, 
-      description: "I'm passionate about backend development. Feel free to connect!", 
+      bio: "I'm passionate about backend development. Feel free to connect!", 
       interests: [
-         "Software", 
-         "Food"
+         {
+            id: "interest1", 
+            name: "Music",
+         },
+         {
+            id: "interest2", 
+            name: "Books",
+         },
+      ], 
+      arts: [
+         {
+            id: "userArt1", 
+            imgSrc: "/images/art-biker-showing-hand.jpeg",
+            imgAlt: "Biker showing hand" 
+         }, 
+         {
+            id: "userArt2",
+            imgSrc: "/images/art-girl-on-motorcycle.webp", 
+            imgAlt: "Girl on motorcycle"
+         }
       ]
    }, 
 ]
