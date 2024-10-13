fa587b9efae3ae57489cf8d0abad55d24b663e49
7FOX7 <kheeugene@gmail.com>
2024-10-13 16:54:32 -0400

    Code cleanup

--- src/components/Profile/General/GeneralContent.jsx
+++ src/components/Profile/General/GeneralContent.jsx
@@ -103,34 +103,3 @@ const GeneralContent = () => {
 }
 
 export default GeneralContent
-
-/*
-
-import successStories from "../../../data/successStories"
-import SuccessStory from "./SuccessStory"
-import Grid2 from "@mui/material/Grid2"
-
-const SuccessStoriesContent = () => {
-   return (
-      <>
-         <Grid2
-            container
-            columns={12}
-            rowSpacing={8}
-            direction="column"   
-            size={12}
-         >
-            {successStories.map((story) => (
-               <SuccessStory
-                  key={story.id} 
-                  story={story}
-               />
-            ))}
-         </Grid2>
-      </>
-   )
-}
-
-export default SuccessStoriesContent
-
-*/
