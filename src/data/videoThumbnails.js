67f2b27cd124f87da8f90d457a348ad00a9cbe24
7FOX7 <kheeugene@gmail.com>
2024-10-13 19:47:11 -0400

    Added new video thumbnails and modified the code accordingly

--- src/data/videoThumbnails.js
+++ src/data/videoThumbnails.js
@@ -1,76 +1,127 @@
 const videoThumbnails = [
    {
       id: "videoThumbnail1",
-      imgSrc: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/AZSo46Cc5Cw?si=1kGaFOmiKlaIt-aN", 
+      imgSrc: "/images/thumbnail-andrews-motorcycle-gear.webp", 
+      imgAlt: "Split image with a biker in gear on right and without on the left", 
+      description: "Staff Picks - Andrew's Motorcycle Gear", 
+      embeddedUrl: "https://www.youtube.com/embed/AZSo46Cc5Cw?si=oA-xxs9icGMkJssi", 
       title: "Youtube video player"
    }, 
    {
       id: "videoThumbnail2",
-      imgSrc: "/images/thumbnail-common-mistakes.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/4maJty0vQjI?si=SXW9xq0FjYKxXfFq", 
+      imgSrc: "/images/thumbnail-energica-ego-first-ride.webp", 
+      imgAlt: "A man sitting on a motorcycle", 
+      description: "Energica Ego First Ride & Review", 
+      embeddedUrl: "https://www.youtube.com/embed/aZAwxu0PFZE?si=AQqehrdY6CZXOQW0", 
       title: "Youtube video player"
    }, 
    {
       id: "videoThumbnail3",
-      imgSrc: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/4maJty0vQjI?si=SXW9xq0FjYKxXfFq", 
+      imgSrc: "/images/thumbnail-50-years-old-full-restoration.webp", 
+      imgAlt: "A man near an old broken motorcycle", 
+      description: "Broken 50 Years Old 1970s HONDA Ruined MotorCycle - Full Restoration", 
+      embeddedUrl: "https://www.youtube.com/embed/HxhzPGTDO-U?si=3UPfUbigjTcVsYhI", 
       title: "Youtube video player"
    }, 
    {
       id: "videoThumbnail4",
-      imgSrc: "/images/thumbnail-common-mistakes.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/4maJty0vQjI?si=SXW9xq0FjYKxXfFq", 
+      imgSrc: "/images/thumbnail-how-to-maintain-motorcycle.webp", 
+      imgAlt: "A man pointing on the motorcycle engine", 
+      description: "How to maintain your motorcycle", 
+      embeddedUrl: "https://www.youtube.com/embed/sRcVGxzdaao?si=KXJ80My5q43BeC94", 
       title: "Youtube video player"
    }, 
    {
       id: "videoThumbnail5",
-      imgSrc: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/4maJty0vQjI?si=SXW9xq0FjYKxXfFq", 
+      imgSrc: "/images/thumbnail-most-affordable-electric-motorcycles.webp", 
+      imgAlt: "A green motorcycle", 
+      description: "Most Affordable Electric Motorcycles From All Over The World", 
+      embeddedUrl: "https://www.youtube.com/embed/-nLqOobKbcg?si=2eImsEP6KTmcBixS", 
       title: "Youtube video player"
    }, 
    {
       id: "videoThumbnail6",
-      imgSrc: "/images/thumbnail-common-mistakes.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/4maJty0vQjI?si=SXW9xq0FjYKxXfFq", 
+      imgSrc: "/images/thumbnail-pro-versus-amateur.webp", 
+      imgAlt: "Two motorbikers staying against each other", 
+      description: "Pro on $2,000 Bike vs Amateur on $20,000 Bike!", 
+      embeddedUrl: "https://www.youtube.com/embed/Pquxa3Y4ZRU?si=oDXymPgsnO2n_fwZ", 
       title: "Youtube video player"
    }, 
    {
       id: "videoThumbnail7",
-      imgSrc: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/4maJty0vQjI?si=SXW9xq0FjYKxXfFq", 
+      imgSrc: "/images/thumbnail-riding-without-gear-is-fun.webp", 
+      imgAlt: "A man riding on a motorcycle without a gear", 
+      description: "Riding a Motorcycle Without Gear is Fun", 
+      embeddedUrl: "https://www.youtube.com/embed/N7e67Jp-4nI?si=2rDuoJSsm-S2N5__", 
       title: "Youtube video player"
    }, 
    {
       id: "videoThumbnail8",
-      imgSrc: "/images/thumbnail-common-mistakes.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/4maJty0vQjI?si=SXW9xq0FjYKxXfFq", 
+      imgSrc: "/images/thumbnail-epic-motorcycle-riding.webp", 
+      imgAlt: "A motorcycle first-person view", 
+      description: "Epic Motorcycle Riding ( EPIC STREET RIDE )", 
+      embeddedUrl: "https://www.youtube.com/embed/KWBWemIUVAA?si=Hf7W_2lEB12-OpD6", 
       title: "Youtube video player"
    }, 
    {
       id: "videoThumbnail9",
-      imgSrc: "/images/thumbnail-andrew's-motorcycle-gear.jpg", 
-      imgAlt: "Video Thumbnail", 
-      description: "Wow, there should be a description", 
-      embeddedUrl: "https://www.youtube.com/embed/4maJty0vQjI?si=SXW9xq0FjYKxXfFq", 
+      imgSrc: "/images/thumbnail-she-quit-job-and-lives-on-motorcycle.webp", 
+      imgAlt: "A woman staying near a motorcycle", 
+      description: "She Quit Her Job & Lives on her Motorcycle Solo Full Time", 
+      embeddedUrl: "https://www.youtube.com/embed/gQzNugryH5U?si=hPeZJsP4RyFRtq7K", 
+      title: "Youtube video player"
+   }, 
+   {
+      id: "videoThumbnail10",
+      imgSrc: "/images/thumbnail-top-5-beginner-mistakes.webp", 
+      imgAlt: "A man riding a bike with a serious face", 
+      description: "Top 5 mistakes beginner Harley riders make in traffic", 
+      embeddedUrl: "https://www.youtube.com/embed/X4GmNM1M0go?si=FIKqHWCWQmDzbafp", 
+      title: "Youtube video player"
+   }, 
+   {
+      id: "videoThumbnail11",
+      imgSrc: "/images/thumbnail-worlds-fastest-motorbike-vs-fabio-wibmer.webp", 
+      imgAlt: "A man in a motorcycle uniform pointing on a motorbike", 
+      description: "World's Fastest Motorbike Vs Fabio Wibmer", 
+      embeddedUrl: "https://www.youtube.com/embed/u1PntXMw_zM?si=5AW5PHmAKR3J9n5P", 
+      title: "Youtube video player"
+   }, 
+   {
+      id: "videoThumbnail12",
+      imgSrc: "/images/thumbnail-why-and-when-to-stand-on-motorcycle.webp", 
+      imgAlt: "A motorbiker losing control on his motorbike", 
+      description: "Tank Slappers and the Quest for Perfect Suspension", 
+      embeddedUrl: "https://www.youtube.com/embed/IzXE32thS1g?si=Ib91NMx8DGrO9BII", 
+      title: "Youtube video player"
+   }, 
+   {
+      id: "videoThumbnail13",
+      imgSrc: "/images/thumbnail-man-builds-anti-rain-motorcycle.webp", 
+      imgAlt: "A motorbiker riding on an anti-rain motorcycle", 
+      description: "A Man Builds DIY ANTI-RAIN Motorcycle", 
+      embeddedUrl: "https://www.youtube.com/embed/eTlDGsJ3Gos?si=DyLr3DUZpQ_1AKxR", 
+      title: "Youtube video player"
+   }, 
+   {
+      id: "videoThumbnail14",   
+      imgSrc: "/images/thumbnail-secret-to-free-motorcycle-gear.webp", 
+      imgAlt: "A man sitting on a motorcycle", 
+      description: "The secret to FREE Motorcycle Gear", 
+      embeddedUrl: "https://www.youtube.com/embed/2BNB2vLXzJU?si=NEMIEB0Q4ChVvwku", 
+      title: "Youtube video player"
+   }, 
+   {
+      id: "videoThumbnail15",
+      imgSrc: "/images/thumbnail-top-five-easy-tricks.webp", 
+      imgAlt: "A motorbiker doing a trick on a motorcycle", 
+      description: "5 Easy Motorcycle Tricks to Learn Pro Skills", 
+      embeddedUrl: "https://www.youtube.com/embed/gCH_l-U0NP8?si=003S-1jZY2PzHt49", 
       title: "Youtube video player"
    }, 
 ]
 
 export default videoThumbnails
+
+/*<iframe width="560" height="315" src="https://www.youtube.com/embed/KWBWemIUVAA?si=Hf7W_2lEB12-OpD6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+*/
