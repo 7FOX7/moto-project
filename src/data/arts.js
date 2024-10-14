62a7b05acef1c5e0647ce10e1697b7b8c48d3a46
7FOX7 <kheeugene@gmail.com>
2024-10-13 23:17:01 -0400

    Added new art-images and updated the array arts

--- src/data/arts.js
+++ src/data/arts.js
@@ -1,48 +1,58 @@
 const arts = [
    {
       id: "art1", 
-      imgSrc: "/images/art-biker-showing-hand.jpeg", 
-      imgAlt: "Biker showing hand"
+      imgSrc: "/images/art-biker-showing-hand.webp", 
+      imgAlt: "A motorbiker showing his hand"
    }, 
    {
       id: "art2", 
       imgSrc: "/images/art-biker-riding-touring-motorcycle-jumps.webp", 
-      imgAlt: "Biker riding touring motorcycle jumps"
+      imgAlt: "A motorbiker riding touring motorcycle jumps"
    },
    {
       id: "art3", 
-      imgSrc: "/images/art-dirt-bike.jpg", 
-      imgAlt: "Dirt bike"
+      imgSrc: "/images/art-dirt-bike.webp", 
+      imgAlt: "A dirt bike"
    }, 
    {
       id: "art4", 
-      imgSrc: "/images/art-empty-motorcycle.jpg", 
-      imgAlt: "Empty motorcycle"
+      imgSrc: "/images/art-empty-motorcycle.webp", 
+      imgAlt: "An empty motorcycle"
    }, 
    {
       id: "art5", 
       imgSrc: "/images/art-girl-on-motorcycle.webp", 
-      imgAlt: "Girl on motorcycle"
+      imgAlt: "A girl on a motorcycle"
    }, 
    {
       id: "art6", 
-      imgSrc: "/images/art-idle-biker.jpg", 
-      imgAlt: "Idle biker"
+      imgSrc: "/images/art-idle-biker.webp", 
+      imgAlt: "An idle biker"
    }, 
    {
       id: "art7", 
-      imgSrc: "/images/art-man-motorcycle.jpg", 
-      imgAlt: "Man riding motorcycle"
+      imgSrc: "/images/art-man-motorcycle.webp", 
+      imgAlt: "A man riding a motorbike"
    }, 
    {
       id: "art8", 
-      imgSrc: "/images/art-standing-girl.jpg", 
-      imgAlt: "Standing girl biker"
+      imgSrc: "/images/art-suzuki-motorbike.webp", 
+      imgAlt: "A Suzuki motorbike"
    }, 
    {
       id: "art9", 
-      imgSrc: "/images/art-empty-motorcycle.jpg", 
-      imgAlt: "Empty motorcycle"
+      imgSrc: "/images/art-girl-riding-a-motorbike-with-the-wind.webp", 
+      imgAlt: "A girl riding a motorbike with the wind"
+   }, 
+   {
+      id: "art10", 
+      imgSrc: "/images/art-woman-sitting-on-a-motorbike.webp", 
+      imgAlt: "A woman sitting on a motorbike and looking down"
+   }, 
+   {
+      id: "art11", 
+      imgSrc: "/images/art-a-winter-soldier-riding-a-motorbike.webp", 
+      imgAlt: "A Winter Soldier riding a motorbike"
    }, 
 ]
 
